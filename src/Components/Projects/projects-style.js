700486f49aeb241e3af5438cd30990234c89d2fc
Samantha Goldstein <samanthagoldstein@Samanthas-MacBook-Pro-2.local>
2021-01-15 15:50:46 -0600

    projects scaffolded

---
+++ src/Components/Projects/projects-style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const ProjectsContainer = styled.div`
+display: grid;
+grid-template-columns: repeat(3, 1fr);
+`;
+
+export const ProjectCard = styled.div`
+text-align: center;
+ width: 420px;
+ height: 620px;
+ background: blue;
+ border-radius: 25px;
+`;
--- src/Components/Projects/projects.js
+++ src/Components/Projects/projects.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React from 'react';
+import {
+  ProjectsContainer,
+  ProjectCard,
+} from './projects-style';
 
 const projects = () => {
   return (
-    <div>
+    <ProjectsContainer>
+      <ProjectCard>
       <h1>This is a project component</h1>
-    </div>
+      <h1>About the Project</h1>
+      <h1>Link to the site</h1>
+      </ProjectCard>
+      <ProjectCard>
+
+      <h1>This is a project component</h1>
+      </ProjectCard>
+      <ProjectCard>
+
+      <h1>This is a project component</h1>
+      </ProjectCard>
+      
+    </ProjectsContainer>
   )
 }
 
