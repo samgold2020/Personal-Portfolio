d7e788eb97a060c3b654a1ba7dd1b4a0bff100de
Samantha Goldstein <samanthagoldstein@Samanthas-MacBook-Pro-2.local>
2021-02-03 15:51:14 -0600

    Skills and Footer components

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -6,17 +6,15 @@ import Navbar from '../Components/Navbar/navbar';
 import Projects from '../Components/Projects/projects';
 import About from '../Components/About/about';
 import Contact from '../Components/Contact/contact';
-import Footer from '../Components/Footer/footer';
-import Skills from '../Components/Skills/skills';
+import Footer from '../Components/Footer/Footer';
+import Skills from '../Components/Skills/Skills';
 import '../App.css';
 
 //Data
 import { ProjectData } from '../Components/Projects/ProjectData';
 
-
 // import Aos from "aos";
 // import "aos/dist/aos.css";
-
 
 const Home = () => {
   // const [ isOpen, setIsOpen ] = useState(false);
@@ -27,7 +25,6 @@ const Home = () => {
 
   return (
     <>
-
       {/* <Sidebar isOpen={isOpen} toggle={toggle}/> */}
       <Navbar />
       <Hero />
