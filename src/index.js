ef741038da5eccc09a0865718e48a5920ccb8943
Samantha Goldstein <samanthagoldstein@Samanthas-MacBook-Pro-2.local>
2021-02-03 12:54:13 -0600

    ReadMe viewing on publish, put index.js in root

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Pages from './Pages/home';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Pages />
   </React.StrictMode>,
   document.getElementById('root')
 );
