5e41abe9322871fabe99e2d812896b60ee6d6827
Samantha Goldstein <samanthagoldstein@Samanthas-MacBook-Pro-2.local>
2021-02-03 13:43:43 -0600

    another publish attempt

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Home from './Pages/home';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
 
 
 
