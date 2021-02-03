6e7f7fcfcf227b4bfd6b11a859a02ca198367d63
Samantha Goldstein <samanthagoldstein@Samanthas-MacBook-Pro-2.local>
2021-02-03 13:25:48 -0600

    added Home component to the root to attempt github page publishing

--- src/App.js
+++ src/App.js
@@ -1,43 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import Hero from '../src/Components/Hero/Hero';
-import Navbar from '../src/Components/Navbar/Navbar';
-import Projects from '../src/Components/Projects/Projects';
-import About from '../src/Components/About/About';
-import Contact from '../src/Components/Contact/Contact';
-import Footer from '../src/Components/Footer/Footer';
-import Skills from '../src/Components/Skills/Skills.js';
-import './App.css';
+import Home from './Pages/home'
 
-//Data
-import { ProjectData } from '../src/Components/Projects/ProjectData';
-
-
-import Aos from "aos";
-import "aos/dist/aos.css";
-
-
-const App = () => {
-  const [ isOpen, setIsOpen ] = useState(false);
-
-  const toggle = () => setIsOpen(!isOpen);
-    
-	useEffect(() => {
-		Aos.init({});
-	}, []);
-
+function App() {
   return (
-    <>
-      {/* <Sidebar isOpen={isOpen} toggle={toggle}/> */}
-      <Navbar toggle={toggle}/>
-      <Hero />
-      <About />
-      <Projects slides={ProjectData}/>
-      <Skills />
-      <Contact />
-      <Footer />
-    </>
-  ) 
+    <Router>
+      <Home />
+    </Router>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
+import Home from './Pages/home'
+
 
 
 ReactDOM.render(
   <Router>
-    <App />
+    <Home />
   </Router>,
   document.getElementById('root')
 );
